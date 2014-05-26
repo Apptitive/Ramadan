d9458f6279be37401f2d5ca15a5f9888774fb245
rayhan <hossain.rayhan@outlook.com>
2014-05-26 19:57:25 +0600

    home screen portrait view is ready..
    Signed-off-by: rayhan <hossain.rayhan@outlook.com>

--- Ramadan/src/main/java/com/appsomehow/ramadan/MainActivity.java
+++ Ramadan/src/main/java/com/appsomehow/ramadan/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
