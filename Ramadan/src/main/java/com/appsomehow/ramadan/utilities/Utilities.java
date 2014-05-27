9dd129b2d8f916dde90793876c0647293e308b7f
AndroidCodeHunter <sharif.iit.du@gmail.com>
2014-05-27 21:25:09 +0600

    apply custom font in checkbox preference

---
+++ Ramadan/src/main/java/com/appsomehow/ramadan/utilities/Utilities.java
@@ -0,0 +1,19 @@
+package com.appsomehow.ramadan.utilities;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+import com.appsomehow.ramadan.R;
+
+/**
+ * Created by Sharif on 5/27/2014.
+ */
+public class Utilities {
+
+
+    public static Typeface getFont(Context context) {
+        return Typeface.createFromAsset(context.getAssets(),
+                context.getString(R.string.font_solaimanlipi));
+    }
+
+}
---
+++ Ramadan/src/main/java/com/appsomehow/ramadan/views/CheckBoxPreferenceAlarm.java
@@ -0,0 +1,42 @@
+package com.appsomehow.ramadan.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.preference.CheckBoxPreference;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import com.appsomehow.ramadan.R;
+import com.appsomehow.ramadan.utilities.Utilities;
+import com.dibosh.experiments.android.support.customfonthelper.AndroidCustomFontSupport;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class CheckBoxPreferenceAlarm extends CheckBoxPreference {
+
+    public CheckBoxPreferenceAlarm(Context context) {
+        super(context);
+    }
+
+    public CheckBoxPreferenceAlarm(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CheckBoxPreferenceAlarm(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+        TextView summaryView = (TextView) view.findViewById(android.R.id.title);
+        summaryView.setText(AndroidCustomFontSupport.getCorrectedBengaliFormat(getContext().getString(R.string.pref_title_new_message_notifications), Utilities.getFont(getContext()), -1));
+    }
+}
