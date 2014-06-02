3fc3cca3c880fe653ab4feb0a2ca4eae3998236e
AndroidCodeHunter <sharif.iit.du@gmail.com>
2014-06-02 17:07:42 +0600

    update character replacement algorithm

--- Ramadan/src/main/java/com/appsomehow/ramadan/utilities/Utilities.java
+++ Ramadan/src/main/java/com/appsomehow/ramadan/utilities/Utilities.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.appsomehow.ramadan.R;
 import com.appsomehow.ramadan.receiver.NotificationCancelReceiver;
@@ -56,17 +57,18 @@ public class Utilities {
         return banglaText;
     }
 
-    public static  String replaceBanglaCharacter(Context context, String word) {
-        word.replace("0",context.getString(R.string.bangla_character_zero));
-        word.replace("1",context.getString(R.string.bangla_character_one));
-        word.replace("2",context.getString(R.string.bangla_character_two));
-        word.replace("3",context.getString(R.string.bangla_character_three));
-        word.replace("4",context.getString(R.string.bangla_character_four));
-        word.replace("5",context.getString(R.string.bangla_character_five));
-        word.replace("6",context.getString(R.string.bangla_character_six));
-        word.replace("7",context.getString(R.string.bangla_character_seven));
-        word.replace("8",context.getString(R.string.bangla_character_eight));
-        word.replace("9",context.getString(R.string.bangla_character_nine));
-        return word;
+    public static String replaceBanglaCharacter(Context context, String word) {
+        String str = new String(word);
+        String newString = str.replace('0', context.getString(R.string.bangla_character_zero).charAt(0));
+        newString = newString.replace('1', context.getString(R.string.bangla_character_one).charAt(0));
+        newString = newString.replace('2', context.getString(R.string.bangla_character_two).charAt(0));
+        newString = newString.replace('3', context.getString(R.string.bangla_character_three).charAt(0));
+        newString = newString.replace('4', context.getString(R.string.bangla_character_four).charAt(0));
+        newString = newString.replace('5', context.getString(R.string.bangla_character_five).charAt(0));
+        newString = newString.replace('6', context.getString(R.string.bangla_character_six).charAt(0));
+        newString = newString.replace('7', context.getString(R.string.bangla_character_seven).charAt(0));
+        newString = newString.replace('8', context.getString(R.string.bangla_character_eight).charAt(0));
+        newString = newString.replace('9', context.getString(R.string.bangla_character_nine).charAt(0));
+        return newString;
     }
 }
