fa6159c3d3e50975342b1655f8ba5806940e44a6
rayhan <hossain.rayhan@outlook.com>
2014-06-09 20:18:02 +0600

    Db Updated

--- Ramadan/src/main/java/com/appsomehow/ramadan/SplashScreen.java
+++ Ramadan/src/main/java/com/appsomehow/ramadan/SplashScreen.java
@@ -2,10 +2,12 @@ package com.appsomehow.ramadan;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewParent;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -16,6 +18,7 @@ public class SplashScreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_splash_screen);
 
         Timer timer = new Timer();
