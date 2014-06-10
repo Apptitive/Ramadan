0dbc7740242c04013b6e5cd1a752294ccfec7f7f
AndroidCodeHunter <sharif.iit.du@gmail.com>
2014-06-10 12:34:08 +0600

    remove home background

--- Ramadan/src/main/java/com/appsomehow/ramadan/MainActivity.java
+++ Ramadan/src/main/java/com/appsomehow/ramadan/MainActivity.java
@@ -12,7 +12,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 
 import com.appsomehow.ramadan.helper.CSVToDbHelper;
 import com.appsomehow.ramadan.helper.DbManager;
