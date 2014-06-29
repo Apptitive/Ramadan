ef7461b931c1e73fd57f82923e6f78010bac5843
rayhan <hossain.rayhan@outlook.com>
2014-06-29 16:17:38 +0600

    Db OnUpgrade() updated.

--- Ramadan/src/main/java/com/apptitive/ramadan/helper/DbHelper.java
+++ Ramadan/src/main/java/com/apptitive/ramadan/helper/DbHelper.java
@@ -3,14 +3,13 @@ package com.apptitive.ramadan.helper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
-
+import com.apptitive.ramadan.R;
 import com.apptitive.ramadan.model.Region;
 import com.apptitive.ramadan.model.TimeTable;
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
-
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,14 +19,16 @@ import java.util.List;
  */
 public class DbHelper extends OrmLiteSqliteOpenHelper {
 
+    private Context context;
     private static final String DATABASE_NAME = "Ramadan.sqlite";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 3;
 
     private Dao<Region, String> regionDao = null;
     private Dao<TimeTable, String> timeTableDao = null;
 
     public DbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context= context;
     }
 
     @Override
@@ -47,11 +48,10 @@ public class DbHelper extends OrmLiteSqliteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, ConnectionSource connectionSource, int oldVersion, int newVersion) {
         List<String> allSql = new ArrayList<String>();
         try {
-            switch (oldVersion) {
-                case 1:
-                    //allSql.add("alter table AdData add column `new_col` VARCHAR");
-                    //allSql.add("alter table AdData add column `new_col2` VARCHAR");
-            }
+                TableUtils.clearTable(connectionSource, Region.class);
+                TableUtils.clearTable(connectionSource, TimeTable.class);
+                CSVToDbHelper.readCSVAndInserIntoDb(context,R.raw.region, DbTableName.Region );
+                CSVToDbHelper.readCSVAndInserIntoDb(context,R.raw.timetable, DbTableName.TimeTable );
             for (String sql : allSql) {
                 sqLiteDatabase.execSQL(sql);
             }
